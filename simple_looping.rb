3cffd745467115f9f172a45f0e0bf67cfe95a2a0
Rafael Cruz <ralph1786@gmail.com>
2018-11-13 20:04:15 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -44,6 +44,7 @@ def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times = 0
   until number_of_times == 7 do
+    
   puts phrase
 end
 
