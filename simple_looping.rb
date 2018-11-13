505302ca3d3799ee9acc484f6b4d829460151cb3
Rafael Cruz <ralph1786@gmail.com>
2018-11-13 20:12:15 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -56,7 +56,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   range = 1..7
-  for phrase in range
+  for value in range
     puts phrase
   end
 end
